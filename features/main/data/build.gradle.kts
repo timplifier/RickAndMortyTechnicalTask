5946f795fbedd643a718b7b18f9ba64a33d8f948
Clar1fy <timplifier@gmail.com>
2022-10-24 01:16:25 +0600

    data build.gradle.kts in main feature

---
+++ features/main/data/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    // Application
+    id(libs.plugins.agp.library.get().pluginId)
+
+    // Kotlin
+    id("kotlin-android")
+
+    // Kapt
+    id("kotlin-kapt")
+}
+
+
+android {
+    namespace = "com.timplifier.data"
+    compileSdk = config.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        minSdk = config.versions.minSdk.get().toInt()
+        targetSdk = config.versions.targetSdk.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName(config.versions.releaseBuildType.get()) {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+
+        getByName(config.versions.debugBuildType.get()) {
+            buildConfigField("String", "BASE_URL", "\"https://rickandmortyapi.com/\"")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = options.versions.kotlinJvmTargetOptions.get()
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(project(":features:main:domain"))
+
+    // Retrofit
+    implementation(libs.bundles.retrofit)
+
+    // OkHttp
+    implementation(libs.bundles.okHttp)
+
+    // Paging
+    api(libs.paging.paging)
+
+    // Room
+    implementation(libs.bundles.room)
+    kapt(libs.room.compiler)
+}
