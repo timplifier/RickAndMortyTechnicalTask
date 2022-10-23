11fb8cfb716d488e246cfcfa483a65a38e1a0bce
Clar1fy <timplifier@gmail.com>
2022-10-24 01:55:30 +0600

    created NetworkModule with already generatedCharactersApiService

---
+++ app/src/main/kotlin/com/timplifier/rickandmortytechnicaltask/di/NetworkModule.kt
@@ -0,0 +1,18 @@
+package com.timplifier.rickandmortytechnicaltask.di
+
+import com.timplifier.data.remote.NetworkClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun generateCharactersApiService(networkClient: NetworkClient) =
+        networkClient.generateCharactersApiService()
+}
