886108a902b3356350f58ad86234521375fc9922
Clar1fy <timplifier@gmail.com>
2022-10-23 22:57:59 +0600

    feat(Kotlin-DSL): migrated to KTS

---
+++ app/build.gradle.kts
@@ -0,0 +1,78 @@
+plugins {
+
+    // Application
+    id(libs.plugins.agp.application.get().pluginId)
+
+    // Kotlin
+    id("kotlin-android")
+
+    // Kapt
+    id("kotlin-kapt")
+
+    // Navigation SafeArgs
+    id(libs.plugins.navigation.safeArgs.get().pluginId)
+
+    // Hilt
+    id(libs.plugins.hilt.android.get().pluginId)
+}
+
+android {
+    namespace = "com.timplifier.rickandmortytechnicaltask"
+    compileSdk = config.versions.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "com.timplifier.rickandmortytechnicaltask"
+        minSdk = config.versions.minSdk.get().toInt()
+        targetSdk = config.versions.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName(config.versions.releaseBuildType.get()) {
+            isMinifyEnabled = false
+            proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
+            )
+        }
+        getByName(config.versions.debugBuildType.get()) {
+            applicationIdSuffix = ".debug"
+            isDebuggable = true
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = options.versions.kotlinJvmTargetOptions.get()
+    }
+
+    //ViewBinding
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    // UI Components
+    implementation(libs.bundles.uiComponents)
+
+    // Core
+    implementation(libs.android.core)
+
+    // Coroutines
+    implementation(libs.bundles.coroutines)
+
+    // Lifecycle
+    implementation(libs.bundles.lifecycle)
+
+    // Navigation
+    implementation(libs.bundles.navigation)
+
+    // Hilt
+    implementation(libs.bundles.hilt)
+    kapt(libs.hilt.compiler)
+}
