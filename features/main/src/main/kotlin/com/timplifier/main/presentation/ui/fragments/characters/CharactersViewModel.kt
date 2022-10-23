868cd16c54d705787e1f186c2c914acfb30205bd
Clar1fy <timplifier@gmail.com>
2022-10-24 01:52:25 +0600

    injected use case; added method to fetchPagedCharacters

--- features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/characters/CharactersViewModel.kt
+++ features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/characters/CharactersViewModel.kt
@@ -1,10 +1,17 @@
 package com.timplifier.main.presentation.ui.fragments.characters
 
 import com.timplifier.core.base.BaseViewModel
+import com.timplifier.domain.useCases.FetchCharactersUseCase
+import com.timplifier.main.presentation.models.toUI
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class CharactersViewModel @Inject constructor() : BaseViewModel() {
-
+class CharactersViewModel @Inject constructor(
+    private val fetchCharactersUseCase: FetchCharactersUseCase
+) :
+    BaseViewModel() {
+    fun fetchCharacters() = fetchCharactersUseCase().gatherPagingRequest {
+        it.toUI()
+    }
 }
