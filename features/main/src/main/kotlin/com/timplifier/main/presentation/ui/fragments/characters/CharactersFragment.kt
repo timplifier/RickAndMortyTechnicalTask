56cee053aae71e13b2608600fea15c1e0929d55e
Clar1fy <timplifier@gmail.com>
2022-10-24 01:17:31 +0600

    characters fragment

---
+++ features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/characters/CharactersFragment.kt
@@ -0,0 +1,15 @@
+package com.timplifier.main.presentation.ui.fragments.characters
+
+import androidx.fragment.app.viewModels
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.timplifier.core.base.BaseFragment
+import com.timplifier.main.R
+import com.timplifier.main.databinding.FragmentCharactersBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CharactersFragment :
+    BaseFragment<FragmentCharactersBinding, CharactersViewModel>(R.layout.fragment_characters) {
+    override val binding by viewBinding(FragmentCharactersBinding::bind)
+    override val viewModel by viewModels<CharactersViewModel>()
+}
---
+++ features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/characters/CharactersViewModel.kt
@@ -0,0 +1,10 @@
+package com.timplifier.main.presentation.ui.fragments.characters
+
+import com.timplifier.core.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CharactersViewModel @Inject constructor() : BaseViewModel() {
+
+}
