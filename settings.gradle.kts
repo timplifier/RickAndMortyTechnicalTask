4c37939af9ae939f832188cd2b17c1299fbe75fa
Clar1fy <timplifier@gmail.com>
2022-10-23 22:46:55 +0600

    feat(Kotlin-DSL): migrated settings.gradle to KTS
    n
    
    n

---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+enableFeaturePreview("VERSION_CATALOGS")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("tim-plifier")
+        maven { url = uri("https://jitpack.io") }
+    }
+    versionCatalogs {
+        create("config") {
+            from(files("gradle/config.versions.toml"))
+        }
+        create("options") {
+            from(files("gradle/options.versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "Rick And Morty Technical Task"
+include(":app")
