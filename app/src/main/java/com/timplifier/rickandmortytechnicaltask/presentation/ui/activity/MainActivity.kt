53a1b8a7eb98206411767f76195e8fcdce246add
Clar1fy <timplifier@gmail.com>
2022-10-23 23:10:25 +0600

    feat(Permissions-&-code-style): cleaned up the manifest, added Internet permission and Access Network State permission

--- app/src/main/java/com/timplifier/rickandmortytechnicaltask/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/timplifier/rickandmortytechnicaltask/presentation/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.timplifier.rickandmortytechnicaltask
+package com.timplifier.rickandmortytechnicaltask.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.timplifier.rickandmortytechnicaltask.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
