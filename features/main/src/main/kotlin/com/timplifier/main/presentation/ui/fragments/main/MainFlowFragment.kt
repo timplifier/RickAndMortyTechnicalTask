79c1f6a1600922e5fc4e3cf44b224d7945be617e
Clar1fy <timplifier@gmail.com>
2022-10-24 01:23:57 +0600

    main flow fragment with its layout and graph

---
+++ features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/main/MainFlowFragment.kt
@@ -0,0 +1,8 @@
+package com.timplifier.main.presentation.ui.fragments.main
+
+import com.timplifier.core.base.BaseFlowFragment
+import com.timplifier.main.R
+
+class MainFlowFragment :
+    BaseFlowFragment(R.layout.fragment_main_flow, R.id.nav_host_fragment_main) {
+}
