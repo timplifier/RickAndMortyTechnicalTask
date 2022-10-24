83b55602fd608caf75de512c0bcf2eaa5a16302e
Clar1fy <timplifier@gmail.com>
2022-10-24 15:05:32 +0600

    feat(Domain/Repositories): added EpisodeRepository

--- features/main/domain/src/main/kotlin/com/timplifier/domain/repositories/CharactersRepository.kt
+++ features/main/domain/src/main/kotlin/com/timplifier/domain/repositories/CharactersRepository.kt
@@ -1,9 +1,9 @@
 package com.timplifier.domain.repository
 
 import androidx.paging.PagingData
-import com.timplifier.domain.models.CharactersModel
+import com.timplifier.domain.models.CharacterModel
 import kotlinx.coroutines.flow.Flow
 
 interface CharactersRepository {
-    fun fetchCharacters(): Flow<PagingData<CharactersModel>>
+    fun fetchCharacters(): Flow<PagingData<CharacterModel>>
 }
---
+++ features/main/domain/src/main/kotlin/com/timplifier/domain/repositories/EpisodeRepository.kt
@@ -0,0 +1,9 @@
+package com.timplifier.domain.repositories
+
+import com.timplifier.common.either.Either
+import com.timplifier.domain.models.EpisodeModel
+import kotlinx.coroutines.flow.Flow
+
+interface EpisodeRepository {
+    fun fetchSingleEpisode(id: Int): Flow<Either<String, EpisodeModel>>
+}
