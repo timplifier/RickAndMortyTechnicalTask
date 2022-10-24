0b7efda03d86bc891a65d807c8b723f75397b2f5
Clar1fy <timplifier@gmail.com>
2022-10-24 18:17:12 +0600

    feat(Remote): changed method arguments from @Query to @Path

--- features/main/data/src/main/kotlin/com/timplifier/data/remote/apiservices/CharacterApiService.kt
+++ features/main/data/src/main/kotlin/com/timplifier/data/remote/apiservices/CharacterApiService.kt
@@ -5,7 +5,7 @@ import com.timplifier.data.remote.dtos.CharacterDto
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface CharactersApiService {
+interface CharacterApiService {
     @GET("api/character")
     suspend fun fetchCharacters(@Query("page") page: Int): BasePagingResponse<CharacterDto>
 }
