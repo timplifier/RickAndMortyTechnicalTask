07f8f2ce95caaff99978e0970d56c2ab2f1d6084
Clar1fy <timplifier@gmail.com>
2022-10-24 15:03:48 +0600

    feat(Domain/Presentation): added Domain and Presentation layers modules

---
+++ features/main/domain/src/main/kotlin/com/timplifier/domain/models/EpisodeModel.kt
@@ -0,0 +1,11 @@
+package com.timplifier.domain.models
+
+data class EpisodeModel(
+    val id: Int,
+    val name: String,
+    val airDate: String,
+    val episode: String,
+    val characters: List<String>,
+    val url: String,
+    val created: String
+)
