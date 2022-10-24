b428fc22ecd33583c1ce4a4de0399a7318e93a54
Clar1fy <timplifier@gmail.com>
2022-10-24 18:25:45 +0600

    style(renaming): renamed use case

--- features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/main/characters/CharactersFragment.kt
+++ features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/main/characters/CharactersFragment.kt
@@ -1,10 +1,19 @@
 package com.timplifier.main.presentation.ui.fragments.main.characters
 
+import android.net.Uri
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.timplifier.core.base.BaseFragment
+import com.timplifier.core.base.BaseLoadStateAdapter
+import com.timplifier.core.extensions.bindViewsToPagingLoadStates
+import com.timplifier.core.extensions.directionsSafeNavigation
+import com.timplifier.core.extensions.loge
 import com.timplifier.main.R
 import com.timplifier.main.databinding.FragmentCharactersBinding
+import com.timplifier.main.presentation.models.toUI
+import com.timplifier.main.presentation.ui.adapters.CharactersAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,6 +21,17 @@ class CharactersFragment :
     BaseFragment<FragmentCharactersBinding, CharactersViewModel>(R.layout.fragment_characters) {
     override val binding by viewBinding(FragmentCharactersBinding::bind)
     override val viewModel by viewModels<CharactersViewModel>()
+    private val charactersAdapter = CharactersAdapter(this::onItemClick, this::fetchFirstSeenIn)
+
+    override fun initialize() {
+        constructRecycler()
+    }
+
+    private fun constructRecycler() = with(binding) {
+        rvCharacters.layoutManager = LinearLayoutManager(requireContext())
+        rvCharacters.adapter = charactersAdapter.withLoadStateFooter(BaseLoadStateAdapter())
+        charactersAdapter.bindViewsToPagingLoadStates(rvCharacters, cpiCharacters)
+    }
 
     override fun launchObservers() {
         subscribeToCharacters()
@@ -19,7 +39,27 @@ class CharactersFragment :
 
     private fun subscribeToCharacters() {
         viewModel.fetchCharacters().spectatePaging {
-
+            charactersAdapter.submitData(it)
         }
     }
+
+    private fun onItemClick(id: Int) {
+        findNavController().directionsSafeNavigation(
+            CharactersFragmentDirections.actionCharactersFragmentToCharacterDetailFragment(
+                id
+            )
+        )
+    }
+
+    private fun fetchFirstSeenIn(position: Int, episodeUrl: String) {
+        viewModel.fetchSingleEpisode(getIdFromEpisodeUrl(episodeUrl))
+            .safeFlowGather(actionIfEitherIsRight = {
+                charactersAdapter.renderCharacterFirstSeenIn(position, it.toUI().name)
+            }, actionIfEitherIsLeft = {
+                loge(msg = it)
+            })
+    }
+
+    private fun getIdFromEpisodeUrl(episodeUrl: String) =
+        Uri.parse(episodeUrl).lastPathSegment.toString().toInt()
 }
--- features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/main/characters/CharactersViewModel.kt
+++ features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/main/characters/CharactersViewModel.kt
@@ -2,16 +2,27 @@ package com.timplifier.main.presentation.ui.fragments.main.characters
 
 import com.timplifier.core.base.BaseViewModel
 import com.timplifier.domain.useCases.FetchCharactersUseCase
+import com.timplifier.domain.useCases.FetchSingleEpisodeUseCase
+import com.timplifier.main.presentation.models.EpisodeUI
 import com.timplifier.main.presentation.models.toUI
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class CharactersViewModel @Inject constructor(
-    private val fetchCharactersUseCase: FetchCharactersUseCase
+    private val fetchCharactersUseCase: FetchCharactersUseCase,
+    private val fetchSingleEpisodeUseCase: FetchSingleEpisodeUseCase
 ) :
     BaseViewModel() {
+    private val _firstSeenInEpisodeState = mutableUiStateFlow<EpisodeUI>()
+    val firstSeenInEpisodeState = _firstSeenInEpisodeState.asStateFlow()
+
     fun fetchCharacters() = fetchCharactersUseCase().gatherPagingRequest {
         it.toUI()
     }
+
+    fun fetchSingleEpisode(id: Int) =
+        fetchSingleEpisodeUseCase(id)
+
 }
