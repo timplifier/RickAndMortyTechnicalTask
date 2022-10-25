3ca4aff95ef94e0a916b8a265ff5f4287efcff3a
Clar1fy <timplifier@gmail.com>
2022-10-25 17:23:16 +0600

    feat(Remote): fetch single character if needed

--- features/main/data/src/main/kotlin/com/timplifier/data/remote/apiservices/CharacterApiService.kt
+++ features/main/data/src/main/kotlin/com/timplifier/data/remote/apiservices/CharacterApiService.kt
@@ -3,9 +3,13 @@ package com.timplifier.data.remote.apiservices
 import com.timplifier.data.base.BasePagingResponse
 import com.timplifier.data.remote.dtos.CharacterDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CharacterApiService {
     @GET("api/character")
     suspend fun fetchCharacters(@Query("page") page: Int): BasePagingResponse<CharacterDto>
+
+    @GET("api/character/{id}")
+    suspend fun fetchSingleCharacter(@Path("id") id: Int): CharacterDto
 }
