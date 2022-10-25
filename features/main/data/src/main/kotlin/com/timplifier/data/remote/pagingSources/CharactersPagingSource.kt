22493b127a2d105b3a81f76e66aafd7dd2d2308a
Clar1fy <timplifier@gmail.com>
2022-10-25 17:21:36 +0600

    feat(Room): also insert character when fetching characters

--- features/main/data/src/main/kotlin/com/timplifier/data/remote/pagingSources/CharactersPagingSource.kt
+++ features/main/data/src/main/kotlin/com/timplifier/data/remote/pagingSources/CharactersPagingSource.kt
@@ -1,13 +1,17 @@
 package com.timplifier.data.remote.pagingSources
 
 import com.timplifier.data.base.BasePagingSource
+import com.timplifier.data.local.db.daos.CharacterDao
 import com.timplifier.data.remote.apiservices.CharacterApiService
 import com.timplifier.data.remote.dtos.CharacterDto
 import com.timplifier.domain.models.CharacterModel
 
 class CharactersPagingSource(
-    private val characterApiService: CharacterApiService
+    private val characterApiService: CharacterApiService,
+    private val characterDao: CharacterDao
 ) :
     BasePagingSource<CharacterDto, CharacterModel>({
-        characterApiService.fetchCharacters(it)
+        characterApiService.fetchCharacters(it).also { characters ->
+            characterDao.insertCharacters(*characters.results.toTypedArray())
+        }
     })
