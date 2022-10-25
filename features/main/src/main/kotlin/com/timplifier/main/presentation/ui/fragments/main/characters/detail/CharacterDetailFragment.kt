f61d772d93e6fcad5b7fc40f384703d0fb3a1939
Clar1fy <timplifier@gmail.com>
2022-10-25 17:16:30 +0600

    feat(CharactersDetail): added logic for detail fragment
    n

--- features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/main/characters/detail/CharacterDetailFragment.kt
+++ features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/main/characters/detail/CharacterDetailFragment.kt
@@ -1,15 +1,52 @@
 package com.timplifier.main.presentation.ui.fragments.main.characters.detail
 
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.timplifier.core.base.BaseFragment
+import com.timplifier.core.extensions.loadImageWithGlide
 import com.timplifier.main.R
 import com.timplifier.main.databinding.FragmentCharacterDetailBinding
+import com.timplifier.main.presentation.receivers.InternetConnectionReceiver
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CharacterDetailFragment :
-    BaseFragment<FragmentCharacterDetailBinding, CharacterDetailViewModel>(R.layout.fragment_character_detail) {
+    BaseFragment<FragmentCharacterDetailBinding, CharacterDetailViewModel>(
+        R.layout.fragment_character_detail,
+    ), InternetConnectionReceiver.InternetConnectionReceiverListener {
+
     override val binding by viewBinding(FragmentCharacterDetailBinding::bind)
     override val viewModel by viewModels<CharacterDetailViewModel>()
+    private val args by navArgs<CharacterDetailFragmentArgs>()
+
+    override fun launchObservers() {
+        subscribeToCharacter()
+        subscribeToLocalCharacter()
+    }
+
+    private fun subscribeToCharacter() = with(binding) {
+        viewModel.characterState.spectateUiState(success = {
+            imCharacter.loadImageWithGlide(it.image)
+            tvCharacterName.text = it.name
+        }, gatherIfSucceed = {
+            it.assembleViewVisibility(gCharacterInfo, cpiCharacterDetail)
+        })
+    }
+
+    private fun subscribeToLocalCharacter() = with(binding) {
+        safeFlowGather {
+            viewModel.localCharacterState.collect {
+                imCharacter.loadImageWithGlide(it?.image)
+                tvCharacterName.text = it?.name
+            }
+        }
+    }
+
+    override fun onNetworkConnectionChanged(isConnected: Boolean) {
+        when (isConnected) {
+            true -> viewModel.fetchSingleCharacter(args.characterId)
+            false -> viewModel.getSingleCharacter(args.characterId)
+        }
+    }
 }
--- features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/main/characters/detail/CharacterDetailViewModel.kt
+++ features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/main/characters/detail/CharacterDetailViewModel.kt
@@ -1,9 +1,37 @@
 package com.timplifier.main.presentation.ui.fragments.main.characters.detail
 
+import androidx.lifecycle.viewModelScope
 import com.timplifier.core.base.BaseViewModel
+import com.timplifier.domain.useCases.FetchSingleCharacterUseCase
+import com.timplifier.domain.useCases.GetSingleCharacterUseCase
+import com.timplifier.main.presentation.models.CharacterUI
+import com.timplifier.main.presentation.models.toUI
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class CharacterDetailViewModel @Inject constructor() : BaseViewModel() {
+class CharacterDetailViewModel @Inject constructor(
+    private val fetchSingleCharacterUseCase: FetchSingleCharacterUseCase,
+    private val getSingleCharacterUseCase: GetSingleCharacterUseCase
+) : BaseViewModel() {
+
+    private val _characterState = mutableUiStateFlow<CharacterUI>()
+    val characterState = _characterState.asStateFlow()
+
+    private val _localCharacterState = MutableStateFlow<CharacterUI?>(null)
+    val localCharacterState = _localCharacterState.asStateFlow()
+
+    fun fetchSingleCharacter(id: Int) =
+        fetchSingleCharacterUseCase(id).gatherRequest(_characterState) { it.toUI() }
+
+    fun getSingleCharacter(id: Int) {
+        viewModelScope.launch {
+            getSingleCharacterUseCase(id).collect {
+                _localCharacterState.value = it.toUI()
+            }
+        }
+    }
 }
