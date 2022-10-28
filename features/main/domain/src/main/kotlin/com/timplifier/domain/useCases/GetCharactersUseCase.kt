53bea7872b4c6abe372ecf9d08f5c9a740f92587
Clar1fy <timplifier@gmail.com>
2022-10-28 17:22:36 +0600

    added name,status & species parameters to use case invoke() method

--- features/main/domain/src/main/kotlin/com/timplifier/domain/useCases/GetCharactersUseCase.kt
+++ features/main/domain/src/main/kotlin/com/timplifier/domain/useCases/GetCharactersUseCase.kt
@@ -6,5 +6,6 @@ import javax.inject.Inject
 class GetCharactersUseCase @Inject constructor(
     private val characterRepository: CharacterRepository
 ) {
-    operator fun invoke() = characterRepository.getLocalCharacters()
+    operator fun invoke(name: String?, status: String?, species: String?, gender: String?) =
+        characterRepository.getLocalCharacters(name, status, species, gender)
 }
