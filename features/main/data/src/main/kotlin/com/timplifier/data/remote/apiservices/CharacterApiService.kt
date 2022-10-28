343d77dc96da74c88ffc9c4cc992bb198cb6ae7f
Clar1fy <timplifier@gmail.com>
2022-10-28 12:26:59 +0600

    added queries for fetchCharacters method

--- features/main/data/src/main/kotlin/com/timplifier/data/remote/apiservices/CharacterApiService.kt
+++ features/main/data/src/main/kotlin/com/timplifier/data/remote/apiservices/CharacterApiService.kt
@@ -8,7 +8,13 @@ import retrofit2.http.Query
 
 interface CharacterApiService {
     @GET("api/character")
-    suspend fun fetchCharacters(@Query("page") page: Int): BasePagingResponse<CharacterDto>
+    suspend fun fetchCharacters(
+        @Query("page") page: Int,
+        @Query("name") name: String?,
+        @Query("status") status: String?,
+        @Query("species") species: String?,
+        @Query("gender") gender: String?
+    ): BasePagingResponse<CharacterDto>
 
     @GET("api/character/{id}")
     suspend fun fetchSingleCharacter(@Path("id") id: Int): CharacterDto
