253c476eb311f0f9856d14ab3f01bc6169fbbe7c
Clar1fy <timplifier@gmail.com>
2022-10-28 12:27:25 +0600

    rebuilt everything for network

--- features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/main/characters/detail/CharacterDetailFragment.kt
+++ features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/main/characters/detail/CharacterDetailFragment.kt
@@ -5,48 +5,50 @@ import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.timplifier.core.base.BaseFragment
 import com.timplifier.core.extensions.loadImageWithGlide
+import com.timplifier.core.utils.InternetConnectivityManager
 import com.timplifier.main.R
 import com.timplifier.main.databinding.FragmentCharacterDetailBinding
-import com.timplifier.main.presentation.receivers.InternetConnectionReceiver
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
 class CharacterDetailFragment :
     BaseFragment<FragmentCharacterDetailBinding, CharacterDetailViewModel>(
         R.layout.fragment_character_detail,
-    ), InternetConnectionReceiver.InternetConnectionReceiverListener {
-
+    ) {
     override val binding by viewBinding(FragmentCharacterDetailBinding::bind)
     override val viewModel by viewModels<CharacterDetailViewModel>()
     private val args by navArgs<CharacterDetailFragmentArgs>()
-
-    override fun launchObservers() {
-        subscribeToCharacter()
-        subscribeToLocalCharacter()
+    private val isConnectedToTheInternet: InternetConnectivityManager by lazy {
+        InternetConnectivityManager(requireActivity())
     }
 
-    private fun subscribeToCharacter() = with(binding) {
-        viewModel.characterState.spectateUiState(success = {
-            imCharacter.loadImageWithGlide(it.image)
-            tvCharacterName.text = it.name
-        }, gatherIfSucceed = {
-            it.assembleViewVisibility(gCharacterInfo, cpiCharacterDetail)
-        })
-    }
-
-    private fun subscribeToLocalCharacter() = with(binding) {
-        safeFlowGather {
-            viewModel.localCharacterState.collect {
-                imCharacter.loadImageWithGlide(it?.image)
-                tvCharacterName.text = it?.name
+    override fun launchObservers(): Unit = with(binding) {
+        isConnectedToTheInternet.observe(viewLifecycleOwner) {
+            viewModel.apply {
+                when (it) {
+                    true -> {
+                        fetchSingleCharacter(args.characterId)
+                        characterState.spectateUiState(success = {
+                            imCharacter.loadImageWithGlide(it.image)
+                            tvCharacterName.text = it.name
+                        }, gatherIfSucceed = {
+                            it.assembleViewVisibility(gCharacterInfo, cpiCharacterDetail)
+                        })
+                    }
+                    else -> {
+                        getSingleCharacter(args.characterId)
+                        safeFlowGather {
+                            localCharacterState.collectLatest {
+                                it?.let { character ->
+                                    imCharacter.loadImageWithGlide(character.image)
+                                    tvCharacterName.text = character.name
+                                }
+                            }
+                        }
+                    }
+                }
             }
-        }
-    }
-
-    override fun onNetworkConnectionChanged(isConnected: Boolean) {
-        when (isConnected) {
-            true -> viewModel.fetchSingleCharacter(args.characterId)
-            false -> viewModel.getSingleCharacter(args.characterId)
         }
     }
 }
