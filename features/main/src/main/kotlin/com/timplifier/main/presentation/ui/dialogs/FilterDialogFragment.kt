637831dfb4558a635ddb2a8fa1223c2f5b770eb6
Clar1fy <timplifier@gmail.com>
2022-10-28 17:20:59 +0600

    completed filter dialog fragment

--- features/main/src/main/kotlin/com/timplifier/main/presentation/ui/dialogs/FilterDialogFragment.kt
+++ features/main/src/main/kotlin/com/timplifier/main/presentation/ui/dialogs/FilterDialogFragment.kt
@@ -2,7 +2,11 @@ package com.timplifier.main.presentation.ui.dialogs
 
 import android.widget.RadioButton
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.timplifier.common.constants.Constants.CHARACTER_GENDER_QUERY
+import com.timplifier.common.constants.Constants.CHARACTER_SPECIES_QUERY
+import com.timplifier.common.constants.Constants.CHARACTER_STATUS_QUERY
 import com.timplifier.core.base.BaseDialogFragmentWithoutViewModel
 import com.timplifier.core.extensions.directionsSafeNavigation
 import com.timplifier.core.extensions.setOnCheckedChangeListenerAndRetrieveItsText
@@ -14,17 +18,28 @@ class FilterDialogFragment :
     BaseDialogFragmentWithoutViewModel<FragmentFilterDialogBinding>(R.layout.fragment_filter_dialog) {
     override val binding by viewBinding(FragmentFilterDialogBinding::bind)
     private val characterFilter = CharacterFilter()
+    private val args by navArgs<FilterDialogFragmentArgs>()
 
-    override fun setupListeners() = with(binding) {
+    override fun initialize() {
+        modifyDialogCancellability()
+    }
+
+    private fun modifyDialogCancellability() {
+        requireDialog().setCancelable(true)
+        requireDialog().setCanceledOnTouchOutside(true)
+    }
+
+    override fun setupListeners() {
         listenToStatusChanges()
         listenToSpeciesChanges()
         listenToGenderChanges()
+        resetFilters()
         applyFilters()
     }
 
     private fun listenToStatusChanges() {
         listenToRadioGroupCheckedStatusAndRetrieveText(
-            "status",
+            CHARACTER_STATUS_QUERY,
             binding.rbAlive,
             binding.rbDead,
             binding.rbUnknown,
@@ -33,7 +48,7 @@ class FilterDialogFragment :
 
     private fun listenToSpeciesChanges() {
         listenToRadioGroupCheckedStatusAndRetrieveText(
-            "species",
+            CHARACTER_SPECIES_QUERY,
             binding.rbHuman,
             binding.rbHumanoid,
             binding.rbAlien,
@@ -42,11 +57,22 @@ class FilterDialogFragment :
 
     private fun listenToGenderChanges() {
         listenToRadioGroupCheckedStatusAndRetrieveText(
-            "gender",
+            CHARACTER_GENDER_QUERY,
             binding.rbMale,
             binding.rbFemale,
             binding.rbUnknownGender,
         )
+    }
+
+    private fun resetFilters() = with(binding) {
+        tvResetFilters.setOnClickListener {
+            rgStatus.clearCheck()
+            rgSpecies.clearCheck()
+            rgGender.clearCheck()
+            characterFilter.status = null
+            characterFilter.species = null
+            characterFilter.gender = null
+        }
     }
 
     private fun applyFilters() {
@@ -63,12 +89,38 @@ class FilterDialogFragment :
         fieldToChange: String,
         vararg radioButtons: RadioButton,
     ) {
-        radioButtons.forEach {
-            it.setOnCheckedChangeListenerAndRetrieveItsText { radioButtonText ->
-                when (fieldToChange) {
-                    "status" -> characterFilter.status = radioButtonText
-                    "species" -> characterFilter.species = radioButtonText
-                    "gender" -> characterFilter.gender = radioButtonText
+        radioButtons.forEach { radioButton ->
+            characterFilter.apply {
+                args.currentFilter?.let { previousFilter ->
+                    when (fieldToChange) {
+                        CHARACTER_STATUS_QUERY -> {
+                            radioButton.isChecked =
+                                radioButton.text.toString() == previousFilter.status
+                            status = previousFilter.status
+                        }
+
+                        CHARACTER_SPECIES_QUERY -> {
+                            radioButton.isChecked =
+                                radioButton.text.toString() == previousFilter.species
+                            species = previousFilter.species
+                        }
+
+                        CHARACTER_GENDER_QUERY -> {
+                            radioButton.isChecked =
+                                radioButton.text.toString() == previousFilter.gender
+                            gender = previousFilter.gender
+                        }
+                    }
+                }
+                radioButton.setOnCheckedChangeListenerAndRetrieveItsText { radioButtonText ->
+                    when (fieldToChange) {
+                        CHARACTER_STATUS_QUERY ->
+                            status = radioButtonText
+                        CHARACTER_SPECIES_QUERY ->
+                            species = radioButtonText
+                        CHARACTER_GENDER_QUERY ->
+                            gender = radioButtonText
+                    }
                 }
             }
         }
