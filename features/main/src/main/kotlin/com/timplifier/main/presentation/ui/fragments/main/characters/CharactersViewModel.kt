2ba9b4201d67289c5f307ed27a78ef58508f94f1
Clar1fy <timplifier@gmail.com>
2022-10-28 17:19:05 +0600

    room logic with queries

--- features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/main/characters/CharactersViewModel.kt
+++ features/main/src/main/kotlin/com/timplifier/main/presentation/ui/fragments/main/characters/CharactersViewModel.kt
@@ -5,6 +5,7 @@ import com.timplifier.core.base.BaseViewModel
 import com.timplifier.domain.useCases.FetchCharactersUseCase
 import com.timplifier.domain.useCases.FetchSingleEpisodeUseCase
 import com.timplifier.domain.useCases.GetCharactersUseCase
+import com.timplifier.domain.useCases.GetSingleEpisodeUseCase
 import com.timplifier.main.presentation.models.CharacterUI
 import com.timplifier.main.presentation.models.toUI
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -18,25 +19,49 @@ import javax.inject.Inject
 class CharactersViewModel @Inject constructor(
     private val fetchCharactersUseCase: FetchCharactersUseCase,
     private val fetchSingleEpisodeUseCase: FetchSingleEpisodeUseCase,
-    private val getCharactersUseCase: GetCharactersUseCase
+    private val getCharactersUseCase: GetCharactersUseCase,
+    private val getSingleEpisodeUseCase: GetSingleEpisodeUseCase
 ) :
     BaseViewModel() {
 
     private val _localCharactersState = MutableStateFlow<List<CharacterUI>>(emptyList())
     val localCharactersState = _localCharactersState.asStateFlow()
 
-    fun fetchCharacters() = fetchCharactersUseCase().gatherPagingRequest {
-        it.toUI()
-    }
+    private val _searchQueryState = MutableStateFlow<String?>(null)
+    val searchQueryState = _searchQueryState.asStateFlow()
+
+    fun fetchCharacters(
+        status: String? = null,
+        species: String? = null,
+        gender: String? = null
+    ) =
+        fetchCharactersUseCase(
+            _searchQueryState.value,
+            status,
+            species,
+            gender
+        ).gatherPagingRequest {
+            it.toUI()
+        }
 
     fun fetchSingleEpisode(id: Int) =
         fetchSingleEpisodeUseCase(id)
 
-    fun getLocalCharacters() {
+    fun getLocalCharacters(
+        status: String? = null,
+        species: String? = null,
+        gender: String? = null
+    ) {
         viewModelScope.launch {
-            getCharactersUseCase().collectLatest {
+            getCharactersUseCase(_searchQueryState.value, status, species, gender).collectLatest {
                 _localCharactersState.value = it.map { characterModel -> characterModel.toUI() }
             }
         }
+    }
+
+    fun getSingleEpisode(url: String) = getSingleEpisodeUseCase(url)
+
+    fun modifySearchQuery(newQuery: String?) {
+        _searchQueryState.value = newQuery
     }
 }
