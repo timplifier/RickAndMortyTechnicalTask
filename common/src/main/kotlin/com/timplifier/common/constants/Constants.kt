3d475e06097ec2f17a13246445c14d014cd30faf
Clar1fy <timplifier@gmail.com>
2022-10-28 17:19:34 +0600

    const field for preferences parameter key and preferences creation along with queries

--- common/src/main/kotlin/com/timplifier/common/constants/Constants.kt
+++ common/src/main/kotlin/com/timplifier/common/constants/Constants.kt
@@ -1,4 +1,10 @@
 package com.timplifier.common.constants
 
 object Constants {
+    const val SHOULD_AWARE_USER_ABOUT_LOST_INTERNET_CONNECTION_KEY =
+        "shouldAwareUserAboutLostInternetConnection"
+    const val RICK_AND_MORTY_TECHNICAL_TASK_PREFERENCES = "rickAndMortyTechnicalTaskPreferences"
+    const val CHARACTER_STATUS_QUERY = "status"
+    const val CHARACTER_SPECIES_QUERY = "species"
+    const val CHARACTER_GENDER_QUERY = "gender"
 }
