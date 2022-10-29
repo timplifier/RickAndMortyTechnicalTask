9f8f7774e407aa40c30d96be57efb056bfa4b6ae
Clar1fy <timplifier@gmail.com>
2022-10-30 03:44:55 +0600

    refactor(extensions): deleted useless things from extension

--- core/src/main/kotlin/com/timplifier/core/extensions/PagingDataAdapterExtensions.kt
+++ core/src/main/kotlin/com/timplifier/core/extensions/PagingDataAdapterExtensions.kt
@@ -10,14 +10,9 @@ import androidx.recyclerview.widget.RecyclerView
 fun <T : Any, VH : RecyclerView.ViewHolder> PagingDataAdapter<T, VH>.bindViewsToPagingLoadStates(
     recyclerView: RecyclerView,
     progressBar: ProgressBar,
-    vararg viewsToBindToLoadStateNotLoading: View = emptyArray(),
 ) {
     addLoadStateListener { loadState ->
         recyclerView.isVisible = loadState.refresh is LoadState.NotLoading
         progressBar.isVisible = loadState.refresh is LoadState.Loading
-        if (itemCount == 0)
-            viewsToBindToLoadStateNotLoading.forEach {
-                it.isVisible = loadState.refresh is LoadState.NotLoading
-            }
     }
 }
