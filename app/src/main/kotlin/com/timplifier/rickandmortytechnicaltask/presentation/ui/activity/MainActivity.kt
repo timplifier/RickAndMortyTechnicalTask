a436a091de8e63dff2aa1417e185010d4a72c7ab
Clar1fy <timplifier@gmail.com>
2022-10-30 04:26:32 +0600

    style(MainActivity.kt): deleted onCreate()

--- app/src/main/kotlin/com/timplifier/rickandmortytechnicaltask/presentation/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/com/timplifier/rickandmortytechnicaltask/presentation/ui/activity/MainActivity.kt
@@ -1,13 +1,8 @@
 package com.timplifier.rickandmortytechnicaltask.presentation.ui.activity
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.timplifier.rickandmortytechnicaltask.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
